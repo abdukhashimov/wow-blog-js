97a3ffc63cdc049e176cda62f6ae3a2e64997c9c
Nuriddin Muhammadjanov <nmuhammadjanov@gmail.com>
2021-05-11 10:23:06 +0500

    id generation changed to uuid

--- models/Feedback.js
+++ models/Feedback.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 const Post = require('./Post');
+const { v4: uuidv4 } = require('uuid');
 
 const feedbackSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        default: uuidv4(),
+    },
     feedback: {
         type: String,
     },
--- models/User.js
+++ models/User.js
@@ -1,12 +1,27 @@
 const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
 
 const userSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        default: uuidv4(),
+    },
     firstname: {
         type: String,
         required: true,
     },
     lastname: {
         type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
     },
     phone_number: {
         type: String,
